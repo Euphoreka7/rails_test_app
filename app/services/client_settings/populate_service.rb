f4310f6ff25a68d6b23da65e1b4c73ca0eff02eb
Emily <euphoreka7@gmail.com>
2023-08-08 17:51:53 +0300

    Less random distribution

--- app/services/client_settings/populate_service.rb
+++ app/services/client_settings/populate_service.rb
@@ -1,22 +1,19 @@
 module ClientSettings
   class PopulateService
     def self.execute(client)
-      Experiment.select("experiments.id as experiment_id, experiment_options.id as option_id, experiment_options.percentage")
-                .joins(:experiment_options)
-                .where("experiments.id not in (select experiment_id from client_settings where client_id = ?)", client.id)
-                .as_json
-                .group_by{|eo| eo['experiment_id']}
-                .each do |experiment_id, options|
-        max_percentage = options.map { |o| o['percentage'] }.sum
-        r_val = rand(0..max_percentage)
-        current_percentage = 0.0
-        options.each do |option|
-          current_percentage += option['percentage']
-          if r_val <= current_percentage
-            client.client_settings.create(experiment_id: experiment_id, experiment_option_id: option['option_id'])
-            break
-          end
-        end
+      Experiment.select(
+          <<~SQL
+            experiments.id as experiment_id, experiment_options.id as option_id, experiment_options.percentage,
+            (select count(*) from client_settings cs where cs.experiment_option_id = experiment_options.id) as options_count
+          SQL
+        )
+        .joins(:experiment_options)
+        .where("experiments.id not in (select experiment_id from client_settings where client_id = ?)", client.id)
+        .as_json
+        .group_by{|eo| eo['experiment_id']}
+        .each do |experiment_id, options|
+          option_id = options.map { |o| [o['option_id'], o['options_count'] / o['percentage']] }.sort_by{ |o| o[1] }[0][0]
+          client.client_settings.create(experiment_id: experiment_id, experiment_option_id: option_id)
       end
     end
   end
